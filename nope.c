78e04e11d3963ed685502878de65ec1605f6f52c
meskana <marcelinusilonuba21@gmail.com>
2023-12-17 07:58:40 -0800

    Stacks, Queues - LIFO, FIFO

---
+++ nope.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *file_nop- nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void file_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
